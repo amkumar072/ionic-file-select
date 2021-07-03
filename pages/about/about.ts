61ffb5e0d48e56fa6126c1d305f023b7a96dde35
amkumar072 <amkumar072@gmail.com>
2021-07-03 14:27:20 +0100

    2 files modified

--- pages/about/about.ts
+++ pages/about/about.ts
@@ -6,9 +6,11 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  public form = [
+    { val: 'Pepperoni', isChecked: true },
+    { val: 'Sausage', isChecked: false },
+    { val: 'Mushroom', isChecked: false }
+  ];
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  constructor(public navCtrl: NavController) {}
 }
